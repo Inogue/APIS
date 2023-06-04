3f6a42db726fce21aea8d4fe20f2bfdac17923e3
Ian <ian.nogueira@enti.cat>
2023-06-05 00:12:26 +0200

    Trabajo

---
+++ textuales.py
@@ -0,0 +1,21 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Button, Label
+
+class HelloWorld(App):
+    	
+	CSS_PATH = "style.css"
+	def compose(self) -> ComposeResult:
+		self.close_button = Button("Close", id="close")
+		yield Label("Hello Textual", id="hello")
+		yield self.close_button
+
+	def on_mount(self) -> None:
+		self.screen.styles.background = "darkblue"
+		self.close_button.styles.background = "red"
+
+	def on_button_pressed(self, event: Button.Pressed) -> None:
+		self.exit(event.button.id)
+
+if __name__ == "__main__":
+	app = HelloWorld()
+	app.run()
